a206e338c18af9e0d54aa37afec0cd4a0e659612
JACFIICayetano <159854188+JACFIICayetano@users.noreply.github.com>
2024-02-24 10:48:12 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,106 +3,56 @@ using namespace std;
 int main() {
     cout << "Hello, World!" << endl;
 
-    //TABLA de 4,
-    /*int numero=4;
+   /* int num1, num2, num3;
+    cout << "Ingresa el primer numero" << endl;
+    cin>> num1;
+    cout << "Ingresa el segundo numero" << endl;
+    cin >> num2;
+    cout << "Ingresa el tercer numero" << endl;
+    cin >> num3;
 
-    cout << "Bienvenido a la Tabla del 4" << endl;
+    if (num1>num2 && num1>num3){
+        cout << "El primer numero es mayor de los tres" << endl;
 
-    for(int i=1; i<=10; i++){
-        cout << numero << "x" <<i<< "=" << numero*i << endl;
-    }*/
-    //TABLA de 6,
-    /*int numero=6;
-
-    cout << "Bienvenido a la Tabla del 6" << endl;
-
-    for(int i=1; i<=10; i++){
-        cout << numero << "x" <<i<< "=" << numero*i << endl;
-    }*/
-
-    // Tabla del 10
-
-    /*int numero=10;
-
-    cout << "Bienvenido a la tabla del 10" << endl;
-
-    for (int m = 1; m <=  10; ++m) {
-        cout << numero << "X" << m << "=" << numero*m << endl;
-
-    }*/
-
-    //FACTORIAL
-
-    /*int numero, factorial=1;
-
-    cout << "Ingrese un numero para generarle el factorial" << endl;
-    cin >> numero;
-
-    for (int i =1; i <=numero; ++i) {
-        factorial = factorial*i;
-
+    } else if (num2>num1 && num2>num3){
+        cout << "El segundo numero es mayor de los tres" << endl;
+    } else if (num3>num1 && num3>num2){
+        cout << "El tercer numero es mayor de los tres" << endl;
     }
-    cout << "El numero factorial es:" << factorial << endl; */
+    else{
+        cout << "Los tres numeros son iguales" << endl;*/
 
-
-
-    // FIBONACCI
-
-    /* int num, a=0, b=1, c=1;
-
-     cout<< "Ingresa el numero de elementos: " << endl;
-     cin >> num;
-
-     for (int i =1; i<num; ++i) {
-         c= a + b;
-         cout << c << " ";
-         a = b;
-         b = c;
-     }
-     cout << "\n";
-     return 0;*/
-
-    // NUMERO PRIMO
-
-    /*int num;
-    bool primo = true;
-
-    cout << "Ingresa un numero" << endl;
-    cin >> num;
-
-    for (int e = 2; e < num; ++e) {
-        if (num % 2 == 0) {
-            primo = false;
-        }
-    }
-    if (primo == true) {
-        cout << "El numero es primo";
-    }else {
-        cout << "El numero no es primo " << endl;
+  /* int numero1,numero2=0, suma;
+    while (numero2>=0){
+        cout << "Ingrese un numero" << endl;
+        cin >> numero1;
+        if (numero1<0){return 0;}
+        cout << "Ingrese un otro numero" << endl;
+        cin >> numero2;
+        if (numero2<0) {return 0;}
+        suma= numero1+numero2;
+        cout << suma << endl;
+        numero1= suma;
     }*/
+  int numer1, y, suma;
+  cout << "Ingresa numeros" << endl;
+  cin >> numer1;
 
-    //NUMERO PAR
+    for (y=2; y<numer1; y+=2) {
+        suma =  suma+y;
+        cout << suma << endl;
+    }
+    if (numer1 % 2== 0){
+        suma = suma + numer1;
+        cout<< suma << endl;
+    }
 
-    /*int nume;
+    return 0;
 
-    cout << "Ingrese un numero " << endl;
-    cin >> nume;
 
-     if (nume % 2 == 0){
-         cout <<"El numero es par";
-     }else{
-         cout << "No es un numero par, Vuelve a Intentarlo" << endl;
-     }
-     return 0;*/
 
-    //TABLA DE MULTIPLICAR CON EL NUMERO INGRESADO
 
-    /*int numero;
 
-    cout << "Ingresa un numero que desees de la tabla de multiplicar" <<  endl;
-    cin >> numero;
-
-     for (int i = 1; i <=10 ; ++i) {
-         cout << numero<< " X " << i << " = " << numero*i << endl;
-     }*/
 }
+
+
